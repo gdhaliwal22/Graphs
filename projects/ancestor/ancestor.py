9403c6b2888981eb5f4eb3fb48fdd2fb74bd52e6
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-04-16 23:17:00 -0700

    ancestor.py working

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -31,11 +31,16 @@ class Graph:
 
 
 def earliest_ancestor(ancestors, starting_node):
+    # Initialize an empty Graph and assign to graph
     graph = Graph()
+    # For each edge (PAIR of vertices) contained in the ancestors input...
     for pair in ancestors:
+        # Add a vertex for each ancestor
         graph.add_vertex(pair[0])
         graph.add_vertex(pair[1])
+        # Add an edge connecting the pair in reverse order (gotta be done this way for graph vertices to connect properly)
         graph.add_edge(pair[1], pair[0])
+    # Initialize an empty Queue and assign to queue
     queue = Queue()
     # Add a [path to] the starting_node in the queue
     queue.enqueue([starting_node])
@@ -49,13 +54,21 @@ def earliest_ancestor(ancestors, starting_node):
         path = queue.dequeue()
         # Assign the path's last vertex [-1] to farthest
         farthest = path[-1]
+        # If this path is greater or equal to the stored path length *and* the farthest vertex has a smaller value than earliest ancestor
+        # (or current path is longer than stored length)
         if (len(path) >= path_length and farthest < earliest_ancestor) or len(path) > path_length:
             # Reassign farthest to the earliest_ancestor
             earliest_ancestor = farthest
             # Reassign length of path to path_length
             path_length = len(path)
+        # For each descendant of the current earliest ancestor (farthest) contained in the graph's vertices...
+        # Must be farthest to accomodate both initial assignment and conditional logic
         for descendant in graph.vertices[farthest]:
+            # Assign a path list to the newly explored path
             new_path = list(path)
+            # Append each descendant to the new_path
             new_path.append(descendant)
+            # Add the new path to our queue
             queue.enqueue(new_path)
+        # Return the earliest ancestor (means queue is now empty)
     return earliest_ancestor
